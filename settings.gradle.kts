c8ffaadb7c2686472cb824b5116dad8676e067a8
Al Sutton <al.sutton@alsutton.com>
2021-02-22 14:01:49 +0000

    Remove unneeded settings entries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,1 @@
-rootProject.name="enterprisepasswordsafe"
-include("app")
-include("lib:accesscontrol")
-include("lib:cryptography")
-include("lib:database:abstraction")
-include("lib:model")
+rootProject.name="java-cryptography-wrapper"
